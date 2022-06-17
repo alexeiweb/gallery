f3649129a40e1fc644f748f6498e7845cfffc803
Alexei Web <alexeiiweb@gmail.com>
2022-06-17 14:48:52 +0300

    start

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+import { getData } from "./getData.js";
+import { renderGallery } from "./renderGallery.js";
+
+const init = async (selectorWrapper) => {
+  const wrapper = document.querySelector(selectorWrapper);
+  const photos = await getData();
+  renderGallery(wrapper, photos);
+};
+
+init('.gallery__wrapper');
---
+++ scripts/renderGallery.js
@@ -0,0 +1,24 @@
+import { createCardPhoto } from "./createCardPhoto.js";
+import { createElem } from "./createElem.js";
+
+export const renderGallery = (wrapper, photos) => {
+  const gallery = createElem('ul', {
+    className: 'grid'
+  });
+  wrapper.append(gallery);
+
+  const grid = new Masonry(gallery, {
+    gutter: 10,
+    itemSecector: '.card',
+    columnWidth: 200,
+    isFitWidth: true,
+  })
+
+  const cards = photos.map(createCardPhoto);
+
+  Promise.all(cards)
+  .then(cards => {
+    gallery.append(...cards);
+    grid.appended(cards)
+  });  
+};
